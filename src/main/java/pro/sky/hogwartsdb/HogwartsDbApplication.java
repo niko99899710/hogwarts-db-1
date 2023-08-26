0272d21fb43a8029f4cf5fb9bf17876aa7df3ced
karinaid <bagdasarova-k@inbox.ru>
2023-08-26 19:22:34 +0400

    Initial commit

---
+++ src/main/java/pro/sky/hogwartsdb/HogwartsDbApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwartsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hogwartsdb/HogwartsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwartsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
