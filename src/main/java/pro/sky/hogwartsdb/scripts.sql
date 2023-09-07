b215a89d55482d09b47f631b5dc4ca4d5ab4599b
karinaid <bagdasarova-k@inbox.ru>
2023-09-07 23:21:38 +0400

    H/W 3.4 part 3

---
+++ src/main/java/pro/sky/hogwartsdb/scripts.sql
@@ -0,0 +1,15 @@
+1. SELECT name, age,  FROM USER
+   WHERE  age > 10 AND  age < 20
+   ORDER BY age
+
+2. SELECT name FROM USER
+
+3. SELECT name, COUNT (name) FROM USER
+   WHERE name IN("o")
+   GROUP BY name
+
+4. SELECT MIN (age), name  FROM USER
+
+
+5. SELECT FROM USER
+   ORDER BY age
